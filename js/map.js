99b6029b7a344ebb863b84b029979f9f956651f2
Danni Dickson <danni.dickson@hotmail.com>
2016-07-29 21:27:37 +1200

    minor tweaks

--- js/map.js
+++ js/map.js
@@ -34,7 +34,5 @@ function initMap() {
 
 function handleLocationError(browserHasGeolocation, infoWindow, pos) {
 	infoWindow.setPosition(pos);
-	infoWindow.setContent(browserHasGeolocation ?
-												'Error: The Geolocation service failed.' :
-												'Error: Your browser doesn\'t support geolocation.');
+	infoWindow.setContent(browserHasGeolocation ? 'Error: The Geolocation service failed.' : 'Error: Your browser doesn\'t support geolocation.');
 }

0cdbfefed34d9cc07e86b787b811b8358e92a06b
Danni Dickson <danni.dickson@hotmail.com>
2016-07-30 15:23:39 +1200

    Calculates the closest safe zone

--- app/js/locator.js
+++ app/js/locator.js
@@ -27,24 +27,57 @@ lc.start();
 
 
 function onLocationFound(e){
+	var location = {
+		lat: e.latitude,
+		long: e.longitude
+	};
+	var route = findQuickestRoute(location);
 
-	findQuickestRoute();
 
-	createNewRoute(e.latitude, e.longitude);
+	createNewRoute(location, route);
 }
 
 function onLocationError(e){
 	window.alert("Oops. Looks like you do not have your Location Services on!");
 }
 
-function findQuicketRoute(){}
+function pythagoras(location){
+	var array = [];
+	for(var i = 0; i < safeZoneData.length; i++){
+		var coords = safeZoneData[i].geometry.coordinates[0][0];
+		var a = location.lat - coords[0];
+		var b = location.long - coords[1];
+		var c = Math.sqrt( a*a + b*b );
 
+		array.push({
+			point: c,
+			lat: coords[0],
+			long: coords[1]
+		});
+	}
+	console.log(array);
+	return array;
+}
+
+function findQuickestRoute(location){
+	var points = pythagoras(location);
+
+	var index = 0;
+	var route = points[0].point;
+	for (var i = 1; i < points.length; i++) {
+		if (points[i].point < route) {
+			route = points[i];
+			index = i;
+		}
+	}
+	return route;
+}
 // Creates a new Route 
-function createNewRoute(lat, long){
+function createNewRoute(location, route){
 	L.Routing.control({
 	    waypoints: [
-	    L.latLng(lat, long),
-	    L.latLng(-39.4928232,176.911848)
+	    L.latLng(location.lat, location.long),
+	    L.latLng(route.lat, route.long)
 	  ],
 	  router: L.Routing.graphHopper('2f1f160d-40d5-4c50-9625-40c20317d3b4'),
 	  lineOptions: {
@@ -53,8 +86,6 @@ function createNewRoute(lat, long){
 	   showAlternatives: true,
 	}).addTo(map);
 }
-
-console.log(safeZones);
 
 map.on("locationerror", onLocationError);
 map.on("locationfound", onLocationFound);
